3916bbf1febe211622426f293800450bf0c6ed59
Igor416 <64898387+Igor416@users.noreply.github.com>
2022-05-29 18:40:46 +0300

    Initial commit

---
+++ vegas_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VegasApiConfig(AppConfig):
+    name = 'vegas_api'
---
+++ vegas_api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+#from .views import RegisterView, LoginView, RestoreView, UserView, LogoutView
+
+urlpatterns = [
+
+]
