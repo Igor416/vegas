c833585a7a2c3e0afcbd245a124b7bce4fc12bd3
Igor Grosu <igrgro@gmail.com>
2022-05-30 09:51:34 +0300

    linked django and react

--- vegas/settings.py
+++ vegas/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vegas_api.apps.VegasApiConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework',
     'corsheaders',
 ]
--- vegas/urls.py
+++ vegas/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vegas_api.urls')),
+    path('api/', include('vegas_api.urls')),
+    path('', include('frontend.urls')),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
