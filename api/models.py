9aa694a19340c33e20adb8c566bf45d93e8da8af
Igor Grosu <igrgro@gmail.com>
2022-06-15 20:27:11 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,28 +1,66 @@
 from django.db import models
 
 # Create your models here.
+def toString(queryset):
+    s = ''
+
+    for record in queryset:
+        s += record.name + ', '
+
+    return s[0:-2]
+
+class Product(models.Model):
+    name = models.CharField("Название", max_length=32)
+
+    def __str__(self):
+        return f'Продукт: "{self.name}"'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+class Choice(models.Model):
+    name = models.CharField("Характеристика", max_length=32)
+    product = models.ManyToManyField(Product)
+    property = models.CharField("Вариант выбора", max_length=32)
+
+    def __str__(self):
+        return f'Вариант выбора для "{self.name}" в "{toString(self.product.all())}": "{self.property}"'
+
+    class Meta:
+        verbose_name = 'вариант выбора'
+        verbose_name_plural = 'варианты выбора'
 
 class Mattrass(models.Model):
     rigidities = [("middlehard", "среднежесткий"), ("average", "средний")]
     collections = [("Comfort", "Comfort"), ("Modern", "Modern")]
     countries = [("Belarus", "Беларусь")]
 
-    type = models.ForeignKey(Type, on_delete=models.RESTRICT)
-    grown_up = models.BooleanField(verbose_name="Для взрослых", default=True)
-    garanty = models.IntegerField(verbose_name="Гарантийный срок")
-    height = models.IntegerField(verbose_name="Высота")
-    max_pressure = models.IntegerField(verbose_name="Макс. нагрузка")
-    rigidity1 = models.CharField(verbose_name="Уровень жесткости стороны 1", choices=rigidities, max_length=30)
-    rigidity2 = models.CharField(verbose_name="Уровень жесткости стороны 2", choices=rigidities, max_length=30)
-    springs = models.IntegerField(verbose_name="Пружины")
-    lifetime = models.IntegerField(verbose_name="Срок Службы")
-    collection = models.CharField(verbose_name="Коллекция", choices=collections, max_length=30)
-    cover = models.BooleanField(verbose_name="Съемный чехол")
-    country = models.CharField(verbose_name="Страна производства", choices=countries, max_length=30)
+    type = models.ManyToManyField(Choice)
+    grown_up = models.BooleanField("Для взрослых", default=True)
+
+    garanty = models.IntegerField("Гарантийный срок")
+    height = models.IntegerField("Высота")
+    max_pressure = models.IntegerField("Макс. нагрузка")
+    rigidity1 = models.CharField("Уровень жесткости стороны 1", choices=rigidities, max_length=32)
+    rigidity2 = models.CharField("Уровень жесткости стороны 2", choices=rigidities, max_length=32)
+    springs = models.IntegerField("Пружины")
+    lifetime = models.IntegerField("Срок Службы")
+    collection = models.CharField("Коллекция", choices=collections, max_length=32)
+    cover = models.BooleanField("Съемный чехол")
+    country = models.CharField("Страна производства", choices=countries, max_length=32)
 
     def __str__(self):
-        return f'Mattrass: "{self.type}"'
+        return f'Матрас: "{self.type}"'
 
     class Meta:
-        verbose_name = 'mattrass'
-        verbose_name_plural = 'mattrasses'
+        verbose_name = 'матрас'
+        verbose_name_plural = 'матрасы'
+
+class Furniture(models.Model):
+    def __str__(self):
+        return f'Мебель: '
+
+    class Meta:
+        verbose_name = 'мебель'
+        verbose_name_plural = 'мебель'
