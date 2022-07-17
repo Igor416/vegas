def46290b2e7ccf29d68e9a8a8adbd7b1fd20320
Igor416 <igrgro@gmail.com>
2022-07-17 16:35:42 +0300

    added EN and filled all fields with 3 languages
    All translations are filled

--- api/translations.py
+++ api/translations.py
@@ -1,39 +1,40 @@
 from enum import Enum
 from . import catalog as ct
 
-RU = 0
-RO = 1
+EN = 0
+RU = 1
+RO = 2
 
 products = {
-    ct.MATTRASS: (('Матрас', 'Матрасы'), ),
-    ct.PILLOW: (('Подушка', 'Подушки'), ),
-    ct.MATTRASSPAD: (('Наматрасник', 'Наматрасники'), ),
-    ct.BLANKET: (('Одеяло', 'Одеяла'), ),
-    ct.BEDSHEETS: (('Постельное белье', 'Постельно белье'), ),
-    ct.BED: (('Кровать', 'Кровати'), ),
-    ct.STAND: (('Тумба', 'Тумбы'), ),
-    ct.BASIS: (('Основание', 'Основания'), )
+    ct.MATTRASS: ((('Mattrass', 'Mattrasses'), ('Матрас', 'Матрасы'), ('Saltea', 'Saltele'))),
+    ct.PILLOW: ((('Pillow', 'Pillows'), ('Подушка', 'Подушки'), ('Perna', 'Perne'))),
+    ct.MATTRASSPAD: ((('Mattrass Pad', 'Mattrass Pads'), ('Наматрасник', 'Наматрасники'), ('Husa', 'Huse'))),
+    ct.BLANKET: ((('Blanket', 'Blankets'), ('Одеяло', 'Одеяла'), ('Plapuma', 'Plapume'))),
+    ct.BEDSHEETS: ((('Bed Sheets', 'Bed Sheets'), ('Постельное белье', 'Постельно белье'), ('Lenjerie de Pat', 'Lenjerie de Pat'))),
+    ct.BED: ((('Bed', 'Beds'), ('Кровать', 'Кровати'), ('Pat', 'Paturi'))),
+    ct.STAND: ((('Stand', 'Stands'), ('Тумба', 'Тумбы'), ('Noptiera', 'Noptiere'))),
+    ct.BASIS: ((('Basis', 'Basises'), ('Основание', 'Основания'), ('Somiera', 'Somiere')))
 }
 
 properties = {
-    ct.MATTRASS_TYPE: ('Тип матраса', ),
-    ct.COLLECTION: ('Коллекция', ),
-    ct.CONSTRUCTION: ('Конструкция', ),
-    ct.RIGIDITY: ('Уровень жесткости стороны', ),
-    ct.SPRINGBLOCK: ('Пружинный блок', ),
-    ct.AGE: ('Для возраста', ),
-    ct.PACKAGE: ('Упаковка', ),
-    ct.MATERIAL_FILLER: ('Материал наполнения', ),
-    ct.COVER: ('Ткань чехла', ),
-    ct.MATTRASSPAD_TYPE: ('Тип наматрасника', ),
-    ct.BINDING: ('Крепление', ),
-    ct.BLANKET_TYPE: ('Тип одеяла', ),
-    ct.BLANKET_COLOR: ('Цвет одеяла', ),
-    ct.FILLING: ('Наполнитель', ),
-    ct.BEDSHEETS_TYPE: ('Тип комплекта', ),
-    ct.BEDSHEETS_COLOR: ('Цвет комплекта', ),
-    ct.TISSUE: ('Ткань', ),
-    ct.BED_TYPE: ('Вид кровати', ),
-    ct.MATERIAL: ('Материал', ),
-    ct.BASIS_TYPE: ('Вид основания', )
+    ct.MATTRASS_TYPE: ('Mattrass Type', 'Тип матраса', 'Tipul de Saltea'),
+    ct.COLLECTION: ('Collection', 'Коллекция', 'Collectie'),
+    ct.CONSTRUCTION: ('Construction', 'Конструкция', 'Constructie'),
+    ct.RIGIDITY: ('Rigidity level of side', 'Уровень жесткости стороны', 'Duritatea laterala'),
+    ct.SPRINGBLOCK: ('Spring block', 'Пружинный блок', 'Bloc de Arc'),
+    ct.AGE: ('Age Category', 'Для возраста', 'Pentru varsta'),
+    ct.PACKAGE: ('Package', 'Упаковка', 'Ambalare'),
+    ct.MATERIAL_FILLER: ('Material Filler', 'Материал наполнения', 'Material de Umplere'),
+    ct.COVER: ('Cover', 'Ткань чехла', 'Material de Acoperire'),
+    ct.MATTRASSPAD_TYPE: ('Mattrass Pad Type', 'Тип наматрасника', 'Tipul de Husa'),
+    ct.BINDING: ('Binding', 'Крепление', 'Fixare'),
+    ct.BLANKET_TYPE: ('Blanket Type', 'Тип одеяла', 'Tipul de Plapume'),
+    ct.BLANKET_COLOR: ('Blanket Color', 'Цвет одеяла', 'Culoare de Plapuma'),
+    ct.FILLING: ('Filling', 'Наполнитель', 'Material de Umplutura'),
+    ct.BEDSHEETS_TYPE: ('Kit Type', 'Тип комплекта', 'Tipul de kit'),
+    ct.BEDSHEETS_COLOR: ('Kit color', 'Цвет комплекта', 'Culoare de kit'),
+    ct.TISSUE: ('Tissue', 'Ткань', 'tesatura'),
+    ct.BED_TYPE: ('Bed Type', 'Вид кровати', 'Tipul de pat'),
+    ct.MATERIAL: ('Material', 'Материал', 'Material'),
+    ct.BASIS_TYPE: ('Basis Type', 'Вид основания', 'Tipul de Somiera')
 }
