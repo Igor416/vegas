6f7777edc75f8b95d688e6047bb0a7cfd345dff6
Igor Grosu <igrgro@gmail.com>
2022-08-06 21:42:23 +0300

    used telegram api and requests mode

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,4 +1,7 @@
-from vegas.settings import BOT_TOKEN, CHANNEL_ID
+from base64 import encode
+import requests
+import json
+from vegas.settings import BOT_TOKEN, BOT_URL, CHANNEL_ID
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,7 +9,11 @@ from rest_framework import status
 
 class OrderCallView(APIView):
     def post(self, request):
-        bot = telegram.Bot(token=BOT_TOKEN)
-        bot.send_message(chat_id=CHANNEL_ID, text=str(request.data))
+        info = request.data
+        data = {
+            'chat_id': str(CHANNEL_ID), 
+            'text': f'Заказан звонок от: {info["name"]}, номер телефона: {info["phone"]}. Продукт: {info["product"]} ({info["category"]})'
+        }
+        r = requests.post(BOT_URL + 'sendMessage', data=data)
         return Response(status=status.HTTP_202_ACCEPTED)
 
--- vegas/settings.py
+++ vegas/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'telegram_api.apps.TelegramBotConfig',
+    'telegram_bot.apps.TelegramBotConfig',
     'frontend.apps.FrontendConfig',
     'rest_framework',
     'corsheaders',
