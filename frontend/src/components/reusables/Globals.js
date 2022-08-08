811846ae50a256bb2ba8a06d9db3c708e0d4d716
Igor Grosu <igrgro@gmail.com>
2022-08-08 21:10:46 +0300

    moved Shops to Globals.js

--- frontend/src/components/reusables/Globals.js
+++ frontend/src/components/reusables/Globals.js
@@ -1,1 +1,23 @@
 export const currencies = ['MDL', 'EUR']
+
+export const Shops = {
+  'Call center': [
+    '079407032'
+  ],
+  'CC Jumbo Showroom': [
+    'Mun. Chișinău, bd. Decebal 23/1',
+    '078 505 520'
+  ],
+  'Magazin Vegas': [
+    'Mun. Chișinău, str. Kiev 16/1',
+    '079 440 057'
+  ],
+  'Showroom Creangă': [
+    'Mun Chișinău, str. Ion Crengă 5/3',
+    '079 840 444'
+  ],
+  'Showroom Ciocana': [
+    'Mun Chișinău, str. Zelinski 7',
+    '079 407 022'
+  ]
+}
