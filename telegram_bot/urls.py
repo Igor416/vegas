e0347d42662ed096eb99168cc8c04eb056d8a72a
Igor Grosu <igrgro@gmail.com>
2022-08-08 18:34:33 +0300

    added OrderView for main order logic

--- telegram_bot/urls.py
+++ telegram_bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderCallView
+from .views import OrderView, OrderCallView
 
 urlpatterns = [
+    path('order/', OrderView.as_view()),
     path('order_call/', OrderCallView.as_view()),
 ]
