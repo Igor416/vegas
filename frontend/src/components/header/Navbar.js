3424aacc739acfad3522c5c71fbb6afa4b1c0568
Igor Grosu <igrgro@gmail.com>
2022-08-09 12:44:29 +0300

    fixed bug with location
    lang change used to return to the initial page no matter what redirects were made

--- frontend/src/components/header/Navbar.js
+++ frontend/src/components/header/Navbar.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SearchBar from "./SearchBar.js";
+import LocationListener from '../reusables/LocationListener.js';
 import { langs as Langs } from '../reusables/Globals.js';
 
 export default function Navbar(props) {
   let langs = {...Langs}
+  let [pathname, setPathname] = useState(location.pathname)
 
   langs = {...langs};
   delete langs[props.lang];
@@ -33,53 +35,56 @@ export default function Navbar(props) {
       cart: 'Coş'
     }
   }
-
+  
   let lang_version = translations[props.lang];
   return (
-    <div className="d-flex align-items-center row px-5 pt-4">
-      <div className="col-1"></div>
-      <div className="col-1">
-        <img style={{ maxWidth: '80%' }} src="/static/images/logo.png"/>
-      </div>
-      <div className="col-3 ps-0">
-        <SearchBar search={lang_version.search} />
-      </div>
-      <div className="col-1 d-flex flex-column align-items-center">
-      {Object.keys(langs).map((lang, index) => {
-      return (
-        <Link key={index} to={location.pathname + '?lang=' + lang}>
-          <button
-            onClick={() => props.updateLang(lang)}
-            className="p-2 bg-white border-0 outline-0 no-hover">
-            <img className="border border-1" style={{ width: "2.5vw" }} src={"/static/images/" + langs[lang] + ".png"}/>
-          </button>
-        </Link>
-      )})}
-      </div>
-      <div className="col-2 text-center border-1 border-start border-end">
-        <span>
-          <FontAwesomeIcon icon='phone' />
-        </span>
-        <br />
-        <span className="h6">{lang_version.order}: <br/>079 40-70-32</span>
-      </div>
-      <div className="col-2 text-center border-end">
-        <span>
-          <FontAwesomeIcon icon='hand-holding-usd' />
-        </span>
-        <br />
-        <span className="h6">{lang_version.credit1} <br/> {lang_version.credit2} </span>
-      </div>
-      <div className="col-1 text-center">
-        <Link to={'/cart' + location.search} className="h6 no-link no-hover">
+    <div>
+      <LocationListener locationChanged={(location) => setPathname(location.pathname)} />
+      <div className="d-flex align-items-center row px-5 pt-4">
+        <div className="col-1"></div>
+        <div className="col-1">
+          <img style={{ maxWidth: '80%' }} src="/static/images/logo.png"/>
+        </div>
+        <div className="col-3 ps-0">
+          <SearchBar search={lang_version.search} />
+        </div>
+        <div className="col-1 d-flex flex-column align-items-center">
+        {Object.keys(langs).map((lang, index) => {
+        return (
+          <Link key={index} to={pathname + '?lang=' + lang}>
+            <button
+              onClick={() => props.updateLang(lang)}
+              className="p-2 bg-white border-0 outline-0 no-hover">
+              <img className="border border-1" style={{ width: "2.5vw" }} src={"/static/images/" + langs[lang] + ".png"}/>
+            </button>
+          </Link>
+        )})}
+        </div>
+        <div className="col-2 text-center border-1 border-start border-end">
           <span>
-            <FontAwesomeIcon icon='shopping-cart' />
+            <FontAwesomeIcon icon='phone' />
           </span>
           <br />
-          <span>{lang_version.cart} <br/> {props.total} ({props.currency})</span>
-        </Link>
+          <span className="h6">{lang_version.order}: <br/>079 40-70-32</span>
+        </div>
+        <div className="col-2 text-center border-end">
+          <span>
+            <FontAwesomeIcon icon='hand-holding-usd' />
+          </span>
+          <br />
+          <span className="h6">{lang_version.credit1} <br/> {lang_version.credit2} </span>
+        </div>
+        <div className="col-1 text-center">
+          <Link to={'/cart' + location.search} className="h6 no-link no-hover">
+            <span>
+              <FontAwesomeIcon icon='shopping-cart' />
+            </span>
+            <br />
+            <span>{lang_version.cart} <br/> {props.total} ({props.currency})</span>
+          </Link>
+        </div>
+        <div className="col-1"></div>
       </div>
-      <div className="col-1"></div>
     </div>
   );
 }
