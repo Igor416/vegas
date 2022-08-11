f0b2bda4123d013d61fb229a2c755d2602f63a17
Igor Grosu <igrgro@gmail.com>
2022-08-11 13:29:46 +0300

    added ReviewView

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BannerView
+from .views import BannerView, ReviewView
 
 urlpatterns = [
     path('', BannerView.as_view()),
+    path('reviews/', ReviewView.as_view()),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListAPIView
-from .serializers import BannerSerializer
-from .models import Banner
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from .serializers import BannerSerializer, ReviewSerializer
+from .models import Banner, Review
 
 # Create your views here.
 class BannerView(ListAPIView):
     queryset = Banner.objects.all()
     serializer_class = BannerSerializer
+
+class ReviewView(ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
