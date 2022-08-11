0cfdc4758403bd323283c61070c2b50f976fc553
Igor Grosu <igrgro@gmail.com>
2022-08-11 11:54:53 +0300

    added content for Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -15,15 +15,27 @@ class Home extends Component {
 
     this.state = {
       lang: this.props.context.lang,
-      banners: []
+      banners: ''
     }
 
     this.translations = {
       en: {
+        is: 'is',
+        chars: [['European components', 'lime-green'], ['Extended warranty up to 25 years', 'dark-cyan'], ['Individual sizes', 'deep-sky-blue']],
+        title: 'At Vegas, we are experts at sleep',
+        desc: 'We believe that good sleep is the foundation of a happy and healthy life, and our dream is that everyone can get their 8 hours of restful sleep.\n\nOur team has developed a wide range of products designed to improve the quality of sleep. Vegas is designed with a visible balance of style and function.'
       },
       ru: {
+        is: 'это',
+        chars: [['Европейские комплектующие', 'lime-green'], ['Увеличенная гарантия до 25 лет', 'dark-cyan'], ['Индивидуальные размеры', 'deep-sky-blue']],
+        title: 'В Vegas у нас есть эксперты по сну',
+        desc: 'Мы верим, что хороший сон является основой счастливой и здоровой жизни, и наша мечта состоит в том, чтобы каждый мог получить свои 8 часов спокойного сна.\n\nНаша команда разработала широкий спектр продуктов, предназначенных для улучшения качества сна. Изделие Vegas создано с видимым балансом стиля и функциональности.'
       },
       ro: {
+        is: 'este',
+        chars: [['Componente europene', 'lime-green'], ['Garanție extinsă până la 25 de ani', 'dark-cyan'], ['Dimensiuni individuale', 'deep-sky-blue']],
+        title: 'La Vegas, suntem experți în somn',
+        desc: 'Credem că un somn bun este baza unei vieți fericite și sănătoase, iar visul nostru este ca toată lumea să își poată avea cele 8 ore de somn odihnitor.\n\nEchipa noastră a dezvoltat o gamă largă de produse concepute pentru a îmbunătăți calitatea somnului. Vegas este proiectat cu un echilibru vizibil de stil și funcție.'
       }
     }
 
@@ -82,8 +94,38 @@ class Home extends Component {
                 </button>
               </div>
               }
+              <div className="col-1"></div>
             </div>
-            <div className="col-1"></div>
+          </div>
+          <div className="d-flex row-nowrap py-5 bg-light justify-content-center align-items-center mt-5">
+            <img src="/static/images/logo.png"/>
+            <span style={{fontWeight: 400}} className="ms-5 h1">{lang_verion.is}</span>
+            <div className="row justify-content-between">
+            {lang_verion.chars.map((char, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  width: '12.5vw',
+                  height: '12.5vw',
+                  fontWeight: 400,
+                  backgroundColor: `var(--${char[1]})`
+                }}
+                className="d-flex p-3 ms-5 justify-content-center align-items-center text-center text-white rounded-circle h5"
+              >
+                <span>{char[0]}</span>
+              </div>
+            )})}
+            </div>
+          </div>
+          <div className="row text-center mt-5 pt-5">
+            <div className="col-3"></div>
+            <div className="col-6">
+              <p className="h2">{lang_verion.title}</p>
+              <br />
+              <p style={{fontWeight: 400, whiteSpace: "pre-line"}}>{lang_verion.desc}</p>
+            </div>
+            <div className="col-3"></div>
           </div>
         </div>
       </div>
