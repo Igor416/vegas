0846781f6d9b978a97a941ae2a9f25acaa064366
Igor Grosu <igrgro@gmail.com>
2022-08-14 19:10:47 +0300

    added translations

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.conf.urls.static import static
+from vegas.settings import BASE_DIR
 from .views import index
 
 urlpatterns = [
     path('', index),
-    path(r'<string>', index),
-    path(r'<string>/<string1>', index),
-    path(r'<string>/<string1>/<string2>', index),
-    path(r'<string>/<string1>/<string2>/<string3>', index)
+    path(r'cart', index),
+    path(r'shops', index),
+    path(r'catalog/<string1>/<string2>', index),
+    path(r'catalog/<string1>/<string2>/<string3>', index),
+    path(r'product/<string1>/<string2>', index),
 ]
+
+urlpatterns += static('/public/locales/dev/', document_root=BASE_DIR.joinpath('frontend/public/locales/en'))
+urlpatterns += static('/public/', document_root=BASE_DIR.joinpath('frontend/public'))
