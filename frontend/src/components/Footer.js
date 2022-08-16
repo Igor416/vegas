6ad395f628e57e0f3ed1e5ea6c7eb94980d9e057
Igor Grosu <igrgro@gmail.com>
2022-08-16 17:23:20 +0300

    added translations for footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,54 +1,63 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withTranslation } from "react-i18next";
+import CustomLink from "./reusables/CustomLink.js";
 
-export default class Footer extends Component {
+class Footer extends Component {
   constructor(props) {
     super(props);
+
+    this.links = {
+      "company": ["about", "story", "certificates", "materials", "jobs"],
+      "store": ["shipping", "contacts", "reviews", "guarantee", "gifts"]
+    }
   }
 
   render() {
+    const t = this.props.t
     return (
-      <div style={{ backgroundColor: 'var(--light-grey)'}} className="container-fluid text-white d-flex justify-content-center">
-        <div className="container row p-5">
-          <div className="col me-5">
-            <div className="mb-2">
-              <span className="h5">Для клиента</span>
-            </div>
-            <span>Магазины</span><br/>
-            <span>Доставка</span><br/>
-            <span>Условия пользования</span><br/>
-            <span>Сертификаты</span><br/>
-            <span>Материалы</span><br/>
-            <span>Акции</span><br/>
-          </div>
-          <div className="col ms-5">
-            <div className="mb-2">
-              <span className="h5">Компания</span>
-            </div>
-            <span>О нас</span>
-          </div>
-          <div className="col me-5">
-            <span>Магазины, а также офис работают круглосуточно с 9 часов утра, до 6 часов вечера, за исключением оффициальных праздничных дней</span>
-          </div>
-          <div className="col ms-5">
-            <div className="mb-2">
-              <span className="h5">Позвоните Нам</span><br/>
-            </div>
-            <span>079 40-70-32</span>
-            <div className="mb-2 mt-4">
-              <span className="h5">Напишите Нам</span><br/>
-            </div>
-            <span>vegasmd.info@gmail.com</span>
-            <div className="mb-2 mt-4">
-              <span className="h5">Подпишитесь на Нас</span><br/>
-            </div>
-            <div className="row">
-              <a target="_blank" href="https://www.facebook.com/MatrasyVegasMoldova/" style={{ color: '#efefef' }} className="no-hover">
-                <i className="fab fa-facebook"></i>
-              </a>
-            </div>
+      <div style={{ borderTop: '1px solid var(--deep-sky-blue)'}} className="container-fluid row p-5">
+        <div className="col-2"></div>
+        {Object.keys(this.links).map((section, index) => {
+        return (
+        <div key={index} className="d-flex flex-column align-items-start col-2">
+          <span className="h5 mb-2">{t(section)}</span>
+          {this.links[section].map((link, index) => {
+          return (
+            <CustomLink to={`/${section}/${link}`} key={index} text={t(link)}/>
+          )})}
+        </div>
+        )})}
+        <div className="d-flex flex-column col-2">
+          <span className="h5 mb-2">{t('phone')}</span>
+          <span>
+            <FontAwesomeIcon icon="phone" /> &nbsp; 079 40-70-32
+          </span>
+          <span className="h5 my-2">{t('mail')}</span>
+          <span>
+            <FontAwesomeIcon icon="envelope" /> &nbsp; vegasmd.info@gmail.com
+          </span>
+          <span className="h5 my-2">{t('follow')}</span>
+          <div className="row h5">
+            <a target="_blank" href="https://www.facebook.com/MatrasyVegasMoldova/" className="link">
+              <FontAwesomeIcon icon={['fab', 'facebook']} color="var(--deep-sky-blue)" />
+            </a>
           </div>
         </div>
+        <div className="col-2">
+          <span>
+            © 2022 ООО «ЛидерМатрасМаркет», ул. Клары Цеткин, 24, пом. 2,<br />
+            Минск, 220004.<br />
+            УНП 190812427<br />
+            Свидетельство No 452, от 07.03.2007г.<br />
+            Выдано: Минским городским исполнительным комитетом<br />
+            Интернет-магазин vegas.by - Регистрация. в ТР №185812 от 03.02.2015 г.
+          </span>
+        </div>
+        <div className="col-2"></div>
       </div>
     );
   }
 }
+
+export default withTranslation('footer')(Footer);
