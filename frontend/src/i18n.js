9349d19be242e81d95fcb7ff2a90f29224be28ec
Igor Grosu <igrgro@gmail.com>
2022-08-16 12:21:09 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next";
 i18n.use(Backend).use(initReactI18next).init({
   lng: 'en',
   react: {
-    useSuspense: true,
+    useSuspense: false,
   },
   supported: ['en', 'ru', 'ro'],
   interpolation: {
