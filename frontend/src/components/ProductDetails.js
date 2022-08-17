1c758e5943b940e7bad83f5a5591b79cb76b9b91
Igor Grosu <igrgro@gmail.com>
2022-08-17 16:06:10 +0300

    added mobile ProductDetail

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -10,7 +10,6 @@ import LocationListener from "./reusables/LocationListener.js";
 import CustomLink from "./reusables/CustomLink.js";
 import SectionImage from "./reusables/SectionImage.js";
 
-
 function withParams(Component) {
   return props => <Component {...props} params={useParams()} context={useOutletContext()} />;
 }
@@ -19,6 +18,7 @@ class ProductDetails extends Component {
   constructor(props) {
     super(props);
 
+    this.isMobile = this.props.context.isMobile;
     this.state = {
       lang: this.props.context.lang,
       currency: this.props.context.currency,
@@ -142,17 +142,17 @@ class ProductDetails extends Component {
       <div>
         <LocationListener locationChanged={this.updateProduct} />
         <SectionImage category={this.state.category} />
-        <div className="container-fluid row px-5 py-4">
-          <div className="col-1" />
+        <div className="container-fluid d-flex mt-5 mb-5 px-2 py-1 mb-sm-0 px-sm-5 py-sm-4">
+          <div className="col-sm-1"></div>
           {this.state.product && 
-          <div className="col-10">
+          <div className="col-sm-10">
             <div className="d-flex flex-column mb-5">
               <div className="d-flex row-nowrap h3">
                 <CustomLink to={`/catalog/${this.state.category.name}/all`} text={this.state.category.name_s}/>
                 <span>&nbsp;{this.state.product.name}</span>
               </div>
-              <div className="d-flex mt-2 row-nowrap align-items-start">
-                <div className="d-flex flex-column border me-5 p-3">
+              <div className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mt-2 align-items-start"}>
+                <div className="d-flex flex-column border me-sm-5 p-3">
                   <div style={{zIndex: 1000}} className="position-absolute d-flex p-3 h4">
                     <div style={{color: (this.state.product.best ? 'gold' : 'var(--light-grey)')}}>
                       <FontAwesomeIcon icon="fa-star"/>
@@ -212,8 +212,8 @@ class ProductDetails extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="d-flex flex-column">
-                  <div className="d-flex border bg-light justify-content-center row-nowrap pt-2 mb-5">
+                <div className="d-flex flex-column w-100">
+                  <div className="d-flex border bg-light justify-content-center row-nowrap pt-2 mt-5 mt-sm-0 mb-sm-5">
                     <div className="border-end p-3 text-end">
                     {this.state.product.discount != 0 && 
                       <div style={{textDecoration: 'line-through'}}>
@@ -297,8 +297,8 @@ class ProductDetails extends Component {
               </div>
             </div>
             <div className="mt-5">
-              <div style={{borderColor: 'var(--deep-sky-blue)'}} className="nav nav-tabs ps-5" id="tab">
-              {['description', 'characteristic', 'structure', 'technologies'].map((tab, index) => {
+              <div style={{borderColor: 'var(--deep-sky-blue)'}} className="nav nav-tabs ps-1 ps-sm-5" id="tab">
+              {['description', 'characteristic', 'structure'].concat(this.isMobile ? [] : ['technologies']).map((tab, index) => {
               if (this.state.product[tab]) {
               return (
                 <button
@@ -313,22 +313,22 @@ class ProductDetails extends Component {
               )}})}
               </div>
               <div className="tab-content" id="tabContent">
-                <div className="flex-column border border-top-0 p-4 tab-pane fade show active" id='description'>
-                  <div className="mb-3" style={{whiteSpace: "pre-line"}}>
+                <div className="flex-column border border-top-0 p-sm-4 tab-pane fade show active" id='description'>
+                  <div className="mb-3 p-2 p-sm-0 mt-3 mt-sm-0" style={{whiteSpace: "pre-line"}}>
                     <span>{this.state.product.desc}</span>
                   </div>
-                  <div className="row mt-3">
-                    <div className="d-flex flex-column justify-content-between col-6 p-4 border">
+                  <div className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mt-3"}>
+                    <div className="d-flex flex-column justify-content-between col-sm-6 p-sm-4">
                       <div>
                       {Object.keys(this.state.product.description).map((key, index) => {
                       return (
-                        <div key={index} className="d-flex mb-2 justify-content-between border-bottom">
-                          <span>{key}</span>
-                          <span className="h6">{this.repr(this.state.product.description[key])}</span>
+                        <div key={index} className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                          <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{key}</span>
+                          <span className="h6 p-2 p-sm-0">{this.repr(this.state.product.description[key])}</span>
                         </div>
                       )})}
                       </div>
-                      <div onClick={() => {
+                      <div className="mt-3 mt-sm-0 mx-auto m-sm-0" onClick={() => {
                         try {
                           $('#tab-characteristic').tab('show');
                           $('#characteristic').tab('show');
@@ -339,73 +339,66 @@ class ProductDetails extends Component {
                         <CustomButton text={t('characteristic')} color="deep-sky-blue" />
                       </div>
                     </div>
-                    <div className="col-6">
+                    <div className="col-sm-6 ">
                       <img src={this.state.product.shortcut} />
                     </div>
                   </div>
                 </div>
-                <div className="row-nowrap border border-top-0 p-4 tab-pane fade" id='characteristic'>
-                  <div className="d-flex flex-column col-6 justify-content-top border-end p-4">
-                    <div className="d-flex mb-2 justify-content-between align-items-center border-bottom">
-                      <span>{t('brand')}</span>
-                      <span className="h6">Vegas</span>
+                <div className={(this.isMobile ? "flex-column" : "row-nowrap") + " border border-top-0 p-sm-4 tab-pane fade"} id='characteristic'>
+                  <div className={(this.isMobile ? "" : "border-end") + "d-flex flex-column col-sm-6 justify-content-top p-sm-4"}>
+                    <div className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                      <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{t('brand')}</span>
+                      <span className="h6 p-2 p-sm-0">Vegas</span>
                     </div>
                     {Object.keys(this.state.product.characteristic).slice(0, 8).map((key, index) => {
                     return (
-                    <div key={index} className="d-flex mb-2 justify-content-between align-items-center border-bottom">
-                      <span>{key}</span>
-                      <span className="h6">{this.repr(this.state.product.characteristic[key])}</span>
+                    <div key={index} className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                      <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{key}</span>
+                      <span className="h6 p-2 p-sm-0">{this.repr(this.state.product.characteristic[key])}</span>
                     </div>
                     )})}
                   </div>
-                  <div className="d-flex flex-column col-6 justify-content-top p-4">
+                  <div className="d-flex flex-column col-sm-6 justify-content-top p-sm-4">
                     {Object.keys(this.state.product.characteristic).slice(8).map((key, index) => {
                     return (
-                    <div key={index} className="d-flex mb-2 justify-content-between align-items-center border-bottom">
-                      <span>{key}</span>
-                      <span className="h6">{this.repr(this.state.product.characteristic[key])}</span>
+                    <div key={index} className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                      <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{key}</span>
+                      <span className="h6 p-2 p-sm-0">{this.repr(this.state.product.characteristic[key])}</span>
                     </div>
                     )})}
                     {['country', 'manufacturer'].map((key, index) => {
                     return (
-                    <div key={index} className="d-flex mb-2 justify-content-between align-items-center border-bottom text-end">
-                      <span>{t(key)}</span>
-                      <span className="h6">{t(key + 'text')}</span>
+                    <div key={index} className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                      <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{t(key)}</span>
+                      <span className="h6 text-end p-2 p-sm-0">{t(key + 'text')}</span>
                     </div>
                     )})}
-                    <div className="d-flex mb-2 justify-content-between align-items-center border-bottom">
-                      <span>{t('note')}</span>
-                      <span className="text-end" style={{fontSize: '0.75em', whiteSpace: "pre-line"}}>{t('notetext')}</span>
+                    <div className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex mb-sm-2 justify-content-between border-bottom"}>
+                      <span style={{backgroundColor: this.isMobile ? 'var(--milk)' : ''}} className="p-2 p-sm-0">{t('note')}</span>
+                      <span className="text-end p-2 p-sm-0" style={{fontSize: '0.75em', whiteSpace: "pre-line"}}>{t('notetext')}</span>
                     </div>
                   </div>
                 </div>
                 <div className="flex-column border border-top-0 p-4 tab-pane fade" id='structure'>
                 {this.state.product.structure.map((layer, index) => {
                 return (
-                  <div key={index} className="row border-bottom py-4">
-                    <div style={{color: 'var(--deep-sky-blue)'}} className="col-2 h5">
+                  <div key={index} className={(this.isMobile ? "flex-column" : "row-nowrap") + " d-flex border-bottom py-4"}>
+                    <div style={{color: 'var(--deep-sky-blue)'}} className="col-sm-3 h5">
                       <span>{layer.name}</span>
                     </div>
-                    <div className="col-2 d-flex align-items-center">
-                      <div
-                        style={{
-                          color: 'white',
-                          backgroundColor: 'var(--deep-sky-blue)',
-                          width: '1.5vw',
-                          height: '1.5vw',
-                          marginLeft: '-0.75vw'
-                        }}
-                        className="rounded-circle d-flex align-items-center justify-content-center position-absolute">
+                    <div className="col-sm-2 d-flex align-items-center">
+                      <div className="rounded-circle d-flex align-items-center justify-content-center position-absolute text-white structure-quantity">
                         <span>{layer.quantity}</span>
                       </div>
                       <img src={layer.image} />
                     </div>
-                    <div className="col-8">
+                    <div className="col-sm-7">
                       <span>{layer.desc}</span>
                     </div>
                   </div>
                 )})}
                 </div>
+                {!this.isMobile &&
                 <div className="flex-column border border-top-0 p-4 tab-pane fade" id='technologies'>
                 {this.state.product.technologies.map((technology, index) => {
                 return (
@@ -424,11 +417,12 @@ class ProductDetails extends Component {
                   </div>
                 )})}
                 </div>
+                }
               </div>
             </div>
           </div>
           }
-          <div className="col-1" />
+          <div className="col-sm-1"></div>
         </div>
       </div>
     );
