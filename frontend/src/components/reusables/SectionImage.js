06909dbd581dbaf0dae26f213c3d0bafcecdb575
Igor Grosu <igrgro@gmail.com>
2022-08-17 14:52:30 +0300

    Update SectionImage.js

--- frontend/src/components/reusables/SectionImage.js
+++ frontend/src/components/reusables/SectionImage.js
@@ -3,16 +3,13 @@ import React from "react";
 export default function SectionImage({category}) {
   return (
     <div
-      className="container-fluid m-0 d-flex flex-column justify-content-around align-items-center"
-      style={{
-        backgroundImage: `url(/static/images/${category.name}.jpg)`,
-        height: '100vh'
-      }}>
+      className="container-fluid m-0 p-0 d-flex flex-column justify-content-around align-items-center"
+      style={{ background: `url(/static/images/${category.name}.jpg) no-repeat center center fixed` }}
+    >
       <div className="container" style={{height: '30vh'}}></div>
       <div
-        className="container bg-light text-dark d-flex flex-column justify-content-center align-items-center"
+        className="container text-dark d-flex flex-column justify-content-center align-items-center"
         style={{
-          opacity: 0.7,
           height: '30vh'
         }}>
         <span className="h4">{category.name_pl}</span>
