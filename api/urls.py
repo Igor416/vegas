a6866625d42df3b4bb787bcd3ef6f1c94f1205ba
Igor Grosu <igrgro@gmail.com>
2022-10-08 19:27:49 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import SearchView, BestView, CategoryView, ProductsView, ProductDetailsView
+from .views import SearchView, BestView, CategoryView, ProductsView, ProductDetailsView, WorkerView
 
 urlpatterns = [
     path('search/', SearchView.as_view()),
     path('best/', BestView.as_view()),
-    path('category/<str:name>/', CategoryView.as_view()),
+    path('category/<str:category>/', CategoryView.as_view()),
     path('products/<str:product>/<str:category>/', ProductsView.as_view()),
     path('products/<str:product>/<str:category>/<str:filter>/', ProductsView.as_view()),
-    path('product/<str:product>/<int:id>/', ProductDetailsView.as_view())
+    path('product/<str:product>/<int:id>/', ProductDetailsView.as_view()),
+    path('worker/', WorkerView.as_view())
 ]
