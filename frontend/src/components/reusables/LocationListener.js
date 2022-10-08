c2b351496c2f19942d21fc48d326685051e6a9d6
Igor Grosu <igrgro@gmail.com>
2022-10-08 21:06:58 +0300

    added titles for pages

--- frontend/src/components/reusables/LocationListener.js
+++ frontend/src/components/reusables/LocationListener.js
@@ -1,11 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export default function LocationListener({locationChanged}) {
+  const [t, i18n] = useTranslation('titles');
+
   let location = useLocation();
+  let prevlocation = usePrevious(location);
   useEffect(() => {
+    if (prevlocation != undefined) {
+      let title = location.pathname.split('/')[1]
+      if (title == '') {
+        title = 'home'
+      }
+      document.title = t(title);
+    }
     locationChanged(location);
   }, [location]);
 
   return <div></div>;
+}
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
 }
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
+    title = request.path.split('/')[1]
+    if title == '':
+        title = 'home'
+    
+    lang = request.GET['lang']
+    context = {
+        'title': requests.get('http://' + request.get_host() + f'/public/locales/{lang}/titles.json').json()[title]
+    }
+    print(context)
+    return render(request, 'frontend/index.html', context=context)
