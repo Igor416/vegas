1358a3baa7a2dd72ea46c9b1c7d4a75f23ecc5d9
Igor Grosu <igrgro@gmail.com>
2022-10-08 16:43:04 +0300

    deleted category desc

--- frontend/src/components/reusables/SectionImage.js
+++ frontend/src/components/reusables/SectionImage.js
@@ -2,19 +2,9 @@ import React from "react";
 
 export default function SectionImage({category}) {
   return (
-    <div
-      className="container-fluid m-0 p-0 d-flex flex-column justify-content-around align-items-center"
-      style={{ background: `url(/static/images/${category.name}.jpg) no-repeat center center fixed` }}
-    >
-      <div className="container" style={{height: '30vh'}}></div>
-      <div
-        className="container text-dark d-flex flex-column justify-content-center align-items-center"
-        style={{
-          height: '30vh'
-        }}>
-        <span className="h4">{category.name_pl}</span>
-        <span className="text-center">{category.desc}</span>
-      </div>
+    <div className="container-fluid m-0 mt-5 p-0 d-flex flex-column justify-content-around align-items-center">
+      <div className="w-100" style={{height: '5vh'}} />
+      <img src={`/static/images/${category.name}.jpg`} style={{width: '100vw'}} />
     </div>
   );
 }
