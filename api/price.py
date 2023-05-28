0a88d14a8251eb56308e3a29b00a0c16125e5a8e
Igor Grosu <igrgro@gmail.com>
2023-05-28 14:36:38 +0300

    create a container for prices

--- api/price.py
+++ api/price.py
@@ -7,16 +7,9 @@ CURRENCIES = {
 class PriceManager:
     def __init__(self, container, request, path=None):
         self.path = path
-        self.container = container
-        if self.path is None:
-            self.EUR = container['priceEUR']
-        else:
-            self.EUR = container[path]['priceEUR']
-        
-        try:
-            self.country = request.COOKIES['country']
-        except:
-            self.country = 'RO'
+        self.container = container if self.path is None else container[path]
+        self.country = request.COOKIES['country']
+        self.EUR = self.container['priceEUR']
 
         self.set_EUR()
         self.add_curr()
@@ -24,14 +17,9 @@ class PriceManager:
     def set_EUR(self):
         self.EUR *= 1.2 if self.country == 'RO' else 1
         self.EUR = round(self.EUR)
-        if self.path is None:
-            self.container['priceEUR'] = self.EUR
-        else:
-            self.container[self.path]['priceEUR'] = self.EUR
+        self.container.update({'price': {'EUR': self.EUR}})
+        del self.container['priceEUR']
 
     def add_curr(self):
         for curr, k in CURRENCIES[self.country]:
-            if self.path is None:
-                self.container['price' + curr] = round(self.EUR * k, 2)
-            else:
-                self.container[self.path]['price' + curr] = round(self.EUR * k, 2)
+            self.container['price'][curr] = round(self.EUR * k, 2)
