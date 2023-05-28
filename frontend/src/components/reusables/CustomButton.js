e1e415f984b5f696e1a671cb4b8727b0d72b9d0b
Igor Grosu <igrgro@gmail.com>
2023-05-28 15:13:30 +0300

    deleted js files

--- frontend/src/components/reusables/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CustomButton({color, text}) {
-  return (
-    <button style={{whiteSpace: 'nowrap'}} className={color + "-button outline-0 p-3 transition"}>
-      <span>{text}</span>
-    </button>
-  );
-}
