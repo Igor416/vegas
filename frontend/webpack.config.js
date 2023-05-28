680a79929a80b772069bc3531b9a1c67291b5f0c
Igor Grosu <igrgro@gmail.com>
2023-05-28 15:14:43 +0300

    replaced js with ts and babel with ts-loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,34 +1,22 @@
-
 const path = require('path');
-const webpack = require('webpack');
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, '../static/frontend'),
-    filename: '[name].js',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, '../static/js'),
+    filename: '[name].js',
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
       },
     ],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        // This has effect on the react lib size
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
-  ],
+  }
 };
