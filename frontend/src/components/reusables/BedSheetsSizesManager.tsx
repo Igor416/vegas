a02e25ca4d23176dc3e85ed61327433312339db1
Igor Grosu <igrgro@gmail.com>
2023-06-09 23:37:38 +0300

    Update BedSheetsSizesManager.tsx

--- frontend/src/components/reusables/BedSheetsSizesManager.tsx
+++ frontend/src/components/reusables/BedSheetsSizesManager.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
+
 import { BedSheetsSize, Size } from "./JSONTypes";
 
 interface BedSheetsSizesManagerProps {
