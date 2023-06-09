357c7fb79709e17d3d026faf2344f76d860a2ca0
Igor Grosu <igrgro@gmail.com>
2023-06-09 23:36:37 +0300

    add detectLang function

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,18 +2,29 @@ import i18n from "i18next";
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from "react-i18next";
 
-export default function geti18n() {
-  let lang = location.search
+export function detectLang(): string {
+  let lang;
+  let search = location.search;
 
-  if (lang == '' || !lang.includes('?lang=')) {
+  if (search.includes('?lang=')) {
+    lang = search.replace('?lang=', '').slice(0, 2);
+  } else {
     lang = navigator.language
     
     if (lang.includes('-')) {
       lang = lang.split('-')[0]
     }
-  } else {
-    lang = lang.replace('?lang=', '')
   }
+  
+  if (search != `?lang=${lang}`) {
+    location.replace(location.pathname + `?lang=${lang}`)
+  }
+  
+  return lang;
+}
+
+export default function geti18n() {
+  let lang = detectLang()
 
   i18n.use(Backend).use(initReactI18next).init({
     lng: lang,
