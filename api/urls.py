083eb486685bbaf71bc6e4a906fc4a8d693442d0
Igor Grosu <igrgro@gmail.com>
2023-06-09 23:16:20 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,9 @@ urlpatterns = [
     path('search/', views.SearchView.as_view()),
     path('best/', views.BestView.as_view()),
     path('mattress_category_prices/', views.MattressColectionsPriceView.as_view()),
-    path('category/<str:category>/', views.CategoryView.as_view()),
-    path('products/<str:product>/<str:category>/', views.ProductsView.as_view()),
-    path('products/<str:product>/<str:category>/<str:filter>/', views.ProductsView.as_view()),
-    path('product/<str:product>/<int:id>/', views.ProductDetailsView.as_view()),
+    path('products/<str:category>/<str:sub_category>/', views.ProductsView.as_view()),
+    path('products/<str:category>/<str:sub_category>/<str:filter>/', views.ProductsView.as_view()),
+    path('product/<str:category>/<int:id>/', views.ProductDetailsView.as_view()),
     path('sales/', views.SalesView.as_view()),
     path('stock/', views.StockView.as_view())
 ]
