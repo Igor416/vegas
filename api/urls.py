a236dda227ed159f9d5cfefc1efd3f8c5ed27530
Igor Grosu <igrgro@gmail.com>
2023-08-09 14:34:03 +0300

    renamed some views

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('search/', views.SearchView.as_view()),
     path('best/', views.BestView.as_view()),
     path('mattress_category_prices/', views.MattressColectionsPriceView.as_view()),
-    path('products/<str:category>/<str:sub_category>/', views.ProductsView.as_view()),
-    path('products/<str:category>/<str:sub_category>/<str:filter>/', views.ProductsView.as_view()),
-    path('product/<str:category>/<int:id>/', views.ProductDetailsView.as_view()),
+    path('products/<str:category>/<str:sub_category>/', views.ListedProductsView.as_view()),
+    path('products/<str:category>/<str:sub_category>/<str:filter>/', views.ListedProductsView.as_view()),
+    path('product/<str:category>/<int:id>/', views.DetailedProductView.as_view()),
     path('sales/', views.SalesView.as_view()),
     path('stock/', views.StockView.as_view())
 ]
