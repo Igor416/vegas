a09b4737c4c9723381947e2a90a8bad53b6c41a8
Igor Grosu <igrgro@gmail.com>
2023-08-09 14:32:21 +0300

    renamed file to detectors

--- api/detectors.py
+++ api/detectors.py
@@ -1,6 +1,6 @@
 def detect_lang(func):
     def method(self, request, *args, **kwargs):
-        lang = request.query_params.get('lang').lower()
+        lang = request.COOKIES.get('lang').lower()
         return func(self, request, lang, *args, **kwargs)
     return method
         
