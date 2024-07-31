12a074a7a21e4a30b31ee05fc1a0413cff4a01eb
Igor Grosu <igrgro@gmail.com>
2024-07-31 12:07:52 +0300

    edited default month

--- stock/models/table.py
+++ stock/models/table.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Table(models.Model):
   year = models.PositiveSmallIntegerField('Год', default=2024)
-  month = models.PositiveSmallIntegerField('Месяц', default=6)
+  month = models.PositiveSmallIntegerField('Месяц', default=8)
 
   def __str__(self):
     return f'Таблица за {self.year}/{"0" if self.month < 10 else ""}{self.month}'
